5c39420c127c9d631d76475c160cbcab682206d2
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2023-01-14 13:15:23 +0530

    Do not allow user to repeat incorrect guesses

--- guess.js
+++ guess.js
@@ -1,32 +1,43 @@
 function guessValue() {
     var userInput = document.getElementById("user-guess").value;
     var userValue = Math.floor(Number(userInput));
-    sendMessage(userValue);
-    if (userValue === targetValue) {
-        endGame();
+    if (userValue > document.getElementById("user-guess").max) {
+        throw new Error(
+            "Please enter an integer less than " +
+                String(document.getElementById("user-guess").max + 1)
+        );
+    } else if (userValue < document.getElementById("user-guess").min) {
+        throw new Error(
+            "Please enter an integer greater than " +
+                String(document.getElementById("user-guess").min - 1)
+        );
     } else {
-        if (userValue > targetValue) {
-            receiveMessage(
-                "The number you are looking for is less than " +
-                    String(userValue)
-            );
+        sendMessage(userValue);
+        if (userValue === targetValue) {
+            endGame();
+        } else {
+            if (userValue > targetValue) {
+                receiveMessage(
+                    "The number you are looking for is less than " +
+                        String(userValue)
+                );
+                document.getElementById("user-guess").max = userValue - 1;
+            } else if (userValue < targetValue) {
+                receiveMessage(
+                    "The number you are looking for is greater than " +
+                        String(userValue)
+                );
+                document.getElementById("user-guess").min = userValue + 1;
+            }
             attempts--;
             if (attempts === 0) {
                 endGame(false);
             }
-            receiveMessage(String(attempts) + " attempts remaining.");
-            document.getElementById("user-guess").max = userValue;
-        } else if (userValue < targetValue) {
-            receiveMessage(
-                "The number you are looking for is greater than " +
-                    String(userValue)
-            );
-            attempts--;
-            if (attempts === 0) {
-                endGame(false);
+            if (attempts === 1) {
+                receiveMessage("1 attempt remaining.");
+            } else {
+                receiveMessage(String(attempts) + " attempts remaining.");
             }
-            receiveMessage(String(attempts) + " attempts remaining.");
-            document.getElementById("user-guess").min = userValue;
         }
     }
 }
